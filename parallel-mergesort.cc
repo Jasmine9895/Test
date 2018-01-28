751d1d36dab2f76869fc4e8105c3b2c27cf75711
random959 <random.yazu@gmail.com>
2018-01-28 11:35:38 -0800

    Cleaned up code,added comments

--- parallel-mergesort.cc
+++ parallel-mergesort.cc
@@ -27,11 +27,7 @@ void seqMerge(keytype* arr,int start,int middle,int end)
 	keytype* B = newKeys(len_b);
 	memcpy(B,arr + middle,len_b*sizeof(keytype));
 
-	//for(int i=0;i<len_a;i++) A[i] = arr[i+start];
-
-	//for(int i=0;i<len_b;i++) B[i] = arr[i+middle];
-
-
+	
 			while(position_a < len_a && position_b < len_b)
 			{
 				if(A[position_a] < B[position_b])
@@ -71,7 +67,7 @@ void seqMerge(keytype* arr,int start,int middle,int end)
 }
 int binary_search(keytype* arr,int val,int len_arr)
 {
-  //Should return int at or lower value
+  //Should return index of array such that arr[index] <= val
   
   int start = 0;
   int end = len_arr-1;
@@ -96,29 +92,26 @@ int binary_search(keytype* arr,int val,int len_arr)
   }
 
 
-  if(start >= len_arr) return start; //Greater than all elements in array
-  else if(end <0) return end; //Lesser than all elements in array
+  if(start >= len_arr) return start; //Greater than all elements in array(=len_arr)
+  else if(end <0) return end; //Lesser than all elements in array - (-1)
   else return ((start+end)/2); //between 2 values
 
 }
 
 void parallel_merge(keytype* arr,int start,int middle,int end)
 {
-    //middle is whether the second array starts
-
+  //middle is whether the second array starts
   //Initialize vectors A and B and copy elements
+  
 
-
-  //not using std lib as will not give corr index
-
-  if(start>= middle || middle>end || start>=end) //changed start to start?=middle
+  if(start>= middle || middle>end || start>=end) 
   {
-    //printf("return from parallel merge\n");
+    
     return;
   } 
   if(end-start==1)
   {
-
+    //This case is when there are only 2 elements to be merged
     if(arr[start] > arr[end])
     {
       int temp = arr[start];
@@ -126,13 +119,6 @@ void parallel_merge(keytype* arr,int start,int middle,int end)
       arr[end]= temp;
 
     }
-
-   // printf("Sorted array is");
-    for(int i=start;i<=end;i++) //printf("%d ", arr[i]);
-
-     // printf("\n");
-    //printf("Returning from parallel_merge\n");
-
     return;
   }
 
@@ -150,28 +136,9 @@ void parallel_merge(keytype* arr,int start,int middle,int end)
   memcpy(B,arr + middle,len_b*sizeof(keytype));
 
   int mid_a = len_a/2;
-  //Use keytype here prob
-  int v = A[mid_a];
-
-
-  //  printf("Values in A: ");
-  //for(int i=0;i<len_a;i++) printf("%d ", A[i]);
- 
-
- // printf("\n");
- // printf("Values in B: ");
-  //for(int i=0;i<len_b;i++)  printf("%d ", B[i]);
-   
- // printf("\n");
-
- // printf("Callling binary_search for value %d at index %d in A\n",v,mid_a);//
-
+  
+  keytype v = A[mid_a];
   int k = binary_search(B,v,len_b);
-
-  //3 cases > all , < all, middle
-  //value returned such that B[k] <=v
- // printf("binary_search result value = %d at index %d in B\n",B[k],k);
-
 
   if(k>=0 && k<len_b)
   {
@@ -203,24 +170,17 @@ void parallel_merge(keytype* arr,int start,int middle,int end)
   }
   else if(k<0)
   {
-   // printf("k<0 case:\n");
-
     //all elements in B are greater than mid-point of A
     //a1b1a2b2, there is no b1, as all elements are greater than mid-point of a, so there is just b2, which includes all of b
     //so we can directly call parallel merge , no need to modify elements , just modify the call(just a1b1b2)
-    int st = start+mid_a+1;
-    int  m= middle;
-    int  e =end;
-
-    //printf("Calling parallel_merge st= arr[%d] = %d, mid =arr[%d]= %d , end = arr[%d]=%d\n",st,arr[st],m,arr[m],e,arr[e]);
-      parallel_merge(arr,start+mid_a+1,middle,end); //mid_a + 1 as have to merge a2b, mid_a is in a1
+   parallel_merge(arr,start+mid_a+1,middle,end); //mid_a + 1 as have to merge a2b, mid_a is in a1
 
   }
   else if(k>=len_b)
   {
       //all elements of b are less than mid_point of A
       //a1b1a2b2, no b2, therefore just a1,b1a2
-    int p = start+mid_a;
+      int p = start+mid_a;
       int q = p+len_b;//here I am assuming that k= len_b
       //int r = q+len_a-mid_a;
 
@@ -236,22 +196,7 @@ void parallel_merge(keytype* arr,int start,int middle,int end)
       parallel_merge(arr,start,p,q-1);
 
   }
- // printf("Completed parallel merge for \n");
- // printf("Values in A: ");
-  //for(int i=0;i<len_a;i++)  printf("%d ", A[i]);
-   
-
- // printf("\n");
- // printf("Values in B: ");
-  //for(int i=0;i<len_b;i++)  printf("%d ", B[i]);
-   
-
- // printf("\n final result\n");
-  //for(int i=start;i<=end;i++)  printf("%d ", arr[i]);
-  
-
-  //printf("\n----------------\n");
-
+ 
 }
 
 void seqMergeSort(keytype* arr, int start,int end)
@@ -272,11 +217,9 @@ void parMergeSort(keytype* arr, int start,int end)
   if(start>=end) return;
   int mid = int((start+end+1)/2);
 
- // printf("Calling merge Sort : start: %lu, mid= %lu, end=%lu\n",start,mid,end);
   parMergeSort(arr,start,mid-1);
   parMergeSort(arr,mid,end); //here mid is the beginning of the second array
   
- // printf("Calling merge: : start: %lu, mid= %lu, end=%lu\n",start,mid,end);
   parallel_merge(arr,start,mid,end);
   return;
 
